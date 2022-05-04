2276ed6bcd412d21bcc7b1ff24ee4b3317ab67d7
Error <88781785+TheSadError@users.noreply.github.com>
2022-05-04 10:38:12 +0400

    Update main.py

--- core/EN/main.py
+++ core/EN/main.py
@@ -3,7 +3,6 @@ import re
 import os
 import time
 from colorama import Fore,Back,Style
-import socket
 from bs4 import BeautifulSoup
 
 class bcolors:
@@ -15,6 +14,7 @@ class bcolors:
 print("Please Select Operation : ")
 print(" ")
 print(f"{bcolors.OK}[1] 2D Line Graf")
+print(f"{bcolors.OK}[2] Pie Graph")
 print(f"{bcolors.OK}[Q] Quit")
 n = input("-> ")
 
@@ -40,9 +40,31 @@ def normal():
     plt.title(name)
     plt.show()
 
+def pie():
+    name = input(f"{bcolors.FAIL}Name of graph : ")
+    x = []
+    xn =  int(input(f"{bcolors.FAIL}How many elements will be in X data: "))
+
+    print("X Datasi : ")
+    for i in range(0, xn):
+        j = int(input())
+        x.append(j)
+    print(f"{bcolors.OK}Getting Inputted Data...")
+    time.sleep(1)
+    print(f"{bcolors.OK}Now, enter the chart view data (0 means combined, 1 separate. And additional information: Chart View data must be the same number as X data.) : ")
+    e=[]
+    print(f"{bcolors.OK}E Data : ")
+    for i in range(0,xn):
+        j = int(input())
+        e.append(j)
+    plt.pie(x, explode = e)
+    plt.title(name)
+    plt.show()
 
 if n == '1':
     normal()
+elif n == '2':
+    pie()
 elif n == 'Q' or n == 'q':
     print(f"{bcolors.FAIL}Thanks you for using ATS...")
     print(f"{bcolors.FAIL}Quitting...")
